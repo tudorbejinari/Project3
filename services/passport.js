b1e9b69975fcedd01019674760495abfeb5eda8e
Tudor Bejinari <tudorbejinari@gmail.com>
2019-05-25 12:27:27 -0600

    add finilized passport

--- services/passport.js
+++ services/passport.js
@@ -20,18 +20,16 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-    
-console.log(profile);
-    User.findOne({googleId: profile.id})
-    .then((existingUser) => {
+}, 
+async (accessToken, refreshToken, profile, done) => {
+    // delete this console
+console.log(profile);   
+  const existingUser = await User.findOne({googleId: profile.id});
 if (existingUser){
-done(null, existingUser);
-} else {
-    new User({ googleId: profile.id}).save()
-    .then(user => done(null, user));
+ return done(null, existingUser);
+} 
+  const user = await new User({ googleId: profile.id}).save();
+   done(null, user);
 }
-    });
-
-})
+)
 );
