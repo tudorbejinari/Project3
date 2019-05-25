adb438758f444143f99f0735f50d605985f81597
Tudor Bejinari <tudorbejinari@gmail.com>
2019-05-25 12:27:27 -0600

    add finilized passport

---
+++ client/src/components/Calculator.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Calculator extends Component {
+    render() {
+        return (
+
+    <button className='btn'>
+        <a href = "/calculator"> Calculator </a>
+    </button>
+
+        );
+    }
+}
+
+export default Calculator;
---
+++ client/src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Landing = ()=> {
+    return (
+        <div style={{textAlign: 'center'}}>
+            <h1>
+               Find Your
+                Dream House!
+                
+            </h1>
+            House Affordability Calculator
+        </div>
+    );
+};
+export default Landing;
