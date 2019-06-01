3abb00b2f970c0581aa6bb22c82cf74c95385392
rargueta92 <rudy.argueta@aol.com>
2019-06-01 10:25:31 -0600

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-=======
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -26,4 +12,3 @@ const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 ReactDOM.render(
 <Provider store={store}><App /></Provider>, 
 document.querySelector('#root'));
->>>>>>> 9660c57ce55d98f7c87a19a3778428f9a18167c8
